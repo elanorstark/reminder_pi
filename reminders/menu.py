9d14c0568ae8e2a8f7fd184903d6e32047dfd5a4
Elanor <els72@cam.ac.uk>
2020-09-11 17:29:56 +0100

    comments added resets position when x pressed

--- reminders/menu.py
+++ reminders/menu.py
@@ -2,7 +2,7 @@ from typing import List
 from reminders.screen import Screen
 
 
-# an item in a menu
+# an item in a menu - used almost like a template
 class MenuItem:
     def __init__(self, name):
         self.name = name
@@ -11,6 +11,7 @@ class MenuItem:
         pass
 
 
+# an item in a menu that does something other than going to another menu
 class ActionItem(MenuItem):
     def __init__(self, name, action):
         super().__init__(name)
@@ -20,18 +21,22 @@ class ActionItem(MenuItem):
         self.action()
 
 
+# an item in a menu that displays another menu when selected
 class MenuList(MenuItem):
     menu_stack = []
 
+    # initialise a MenuList
     def __init__(self, name: str, items: List[MenuItem]):
         super().__init__(name)
         self.items: List[MenuItem] = [ActionItem("..", MenuList.back)] + items
         self.position = 0
 
+    # adds menu to the stack when selected
     def selected(self):
         MenuList.menu_stack.append(self)
         self.position = 0
 
+    # displays menu on screen
     def display(self):
         text = self.name + "\n" + ("-" * len(self.name)) + "\n"
         for i, item in enumerate(self.items):
@@ -42,6 +47,8 @@ class MenuList(MenuItem):
         print(text)
         Screen.text_screen(text)
 
+    # decides what to do depending on which button was pressed
+    # a = select, b = up menu, y = down menu, x = home screen
     def handle_button_press(self, button):
         if button == "a":
             # select
@@ -57,15 +64,19 @@ class MenuList(MenuItem):
         elif button == "x":
             # home/toplevel button
             MenuList.menu_stack = MenuList.menu_stack[:1]
+            self.position = 0
 
+    # returns current menu, ie top of stack
     @staticmethod
     def current():
         return MenuList.menu_stack[-1]
 
+    # adds the top level menu to the stack
     @staticmethod
     def initialise(menu):
         MenuList.menu_stack = [menu]
 
+    # when back button is pressed - go back to previous level of menu
     @staticmethod
     def back():
         if len(MenuList.menu_stack) > 1:
